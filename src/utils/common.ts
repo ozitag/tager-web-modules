0e07eac533c42b686ee1c404b5c3f1dc60020a50
Vital Ozierski <vital@ozerich.com>
2022-09-24 18:28:01 +0300

    0.1.12 - add sleep function to utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,62 +1,62 @@
 import React from 'react';
-import { Nullish } from '@tager/web-core';
-import { Page, PictureImageType, PictureProps } from '@tager/web-components';
+import {Nullish} from '@tager/web-core';
+import {Page, PictureImageType, PictureProps} from '@tager/web-components';
 
 import {
-  SeoParamsType,
-  SettingsItemCommon,
-  ThumbnailType,
+    SeoParamsType,
+    SettingsItemCommon,
+    ThumbnailType,
 } from '../typings/models';
 
 type PageProps = React.ComponentProps<typeof Page>;
 
 export function convertSeoParamsToPageProps(
-  seoData: Nullish<SeoParamsType>
+    seoData: Nullish<SeoParamsType>
 ): PageProps {
-  return {
-    title: seoData?.title,
-    description: seoData?.description,
-    keywords: seoData?.keywords,
-    openGraphImage: seoData?.openGraphImage,
-  };
+    return {
+        title: seoData?.title,
+        description: seoData?.description,
+        keywords: seoData?.keywords,
+        openGraphImage: seoData?.openGraphImage,
+    };
 }
 
-export type NarrowAction<
-  Item,
-  Key extends string
-> = Item extends SettingsItemCommon<Key> ? Item : never;
+export type NarrowAction<Item,
+    Key extends string> = Item extends SettingsItemCommon<Key> ? Item : never;
 
-export function getSettingValueByKey<
-  T extends SettingsItemCommon,
-  K extends T['key']
->(settingsList: Array<T>, key: K): NarrowAction<T, K>['value'] | null {
-  const foundItem = settingsList.find((settingItem) => settingItem.key === key);
+export function getSettingValueByKey<T extends SettingsItemCommon,
+    K extends T['key']>(settingsList: Array<T>, key: K): NarrowAction<T, K>['value'] | null {
+    const foundItem = settingsList.find((settingItem) => settingItem.key === key);
 
-  return foundItem ? foundItem.value : null;
+    return foundItem ? foundItem.value : null;
 }
 
 export function convertThumbnailToPictureProps(
-  image: Nullish<ThumbnailType>
+    image: Nullish<ThumbnailType>
 ): PictureProps<'string'> | undefined {
-  if (!image) return undefined;
+    if (!image) return undefined;
 
-  return {
-    src: image.url,
-    src2x: image.url_2x,
-    srcWebp: image.url_webp,
-    srcWebp2x: image.url_webp_2x,
-  };
+    return {
+        src: image.url,
+        src2x: image.url_2x,
+        srcWebp: image.url_webp,
+        srcWebp2x: image.url_webp_2x,
+    };
 }
 
 export function convertThumbnailToPictureImage(
-  image: Nullish<ThumbnailType>
+    image: Nullish<ThumbnailType>
 ): PictureImageType | undefined {
-  if (!image) return undefined;
+    if (!image) return undefined;
 
-  return {
-    src: image.url,
-    src2x: image.url_2x,
-    webp: image.url_webp,
-    webp2x: image.url_webp_2x,
-  };
+    return {
+        src: image.url,
+        src2x: image.url_2x,
+        webp: image.url_webp,
+        webp2x: image.url_webp_2x,
+    };
+}
+
+export function sleep(ms: number) {
+    return new Promise((r) => setTimeout(r, ms));
 }
